31c3f747f34cd76b91b736e21edd850ad927ab11
NilAngelats <nilangelatsregincos@gmail.com>
2026-03-04 13:05:54 +0100

    Initial structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Destinations from "./pages/Destinations";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
